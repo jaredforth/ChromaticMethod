740c54c7f65f5da60340ea5fb5340affb39a9d69
Jared Forth <jforth22@gmail.com>
2018-04-21 18:38:28 -0500

    Key of C pages built

---
+++ ChromaticMethod/CChordFinder.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ChromaticMethod
+{
+    public partial class CChordFinder : ContentPage
+    {
+        public CChordFinder()
+        {
+            InitializeComponent();
+        }
+    }
+}
