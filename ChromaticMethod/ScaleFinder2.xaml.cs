d098ea2b610758f896c8833e8f27ee50a352629a
Jared Forth <jforth22@gmail.com>
2018-04-21 18:31:02 -0500

    C homepage navigation

---
+++ ChromaticMethod/ScaleFinder2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ChromaticMethod
+{
+    public partial class ScaleFinder2 : ContentPage
+    {
+        public ScaleFinder2()
+        {
+            InitializeComponent();
+        }
+
+        void C_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("C Major Scale", "C - D - E ...", "Go Back");
+        }
+    }
+}
