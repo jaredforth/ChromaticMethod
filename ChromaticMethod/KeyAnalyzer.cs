429664eaf6da96a8a7eaf503cb2faca2ef2b24bc
Jared Forth <jforth22@gmail.com>
2018-04-29 16:29:16 -0500

    rough draft of key analyzer

--- ChromaticMethod/KeyAnalyzer.cs
+++ ChromaticMethod/KeyAnalyzer.cs
@@ -20,17 +20,20 @@ namespace ChromaticMethod
             var layout = new StackLayout();
             var header = new Label() { Text = "Key Analyzer", TextColor = Color.Black, FontSize = 30, HorizontalOptions = LayoutOptions.Center };
             var subHeader = new Label { Text = "Overview", FontSize = 20, HorizontalOptions = LayoutOptions.Center, Margin = 20 };
-            var searchheader = new Label { Text = $"Search key of {Key}", FontSize = 20, HorizontalOptions = LayoutOptions.Center, Margin = 20 };
+            var searchHeader = new Label { Text = "Search", FontSize = 20, HorizontalOptions = LayoutOptions.Center, Margin = 20 };
 
             layout.Children.Add(header);
             layout.Children.Add(subHeader);
 
             if (Key == "C")
             {
-                var scale = new Label() {Text = $"The notes in the {Key} major scale are {Major.ModeI(Key)}"};
+                var scale = new Label() { Text = $"The notes in the {Key} major scale are {Major.ModeI(Key)}" };
                 var chords = new Label() { Text = $"The {Key} diatonic triads are {Triads.TriadI(Key)}" };
+                var search = new Button { Text = $"Search Key of {Key}" };
                 layout.Children.Add(scale);
                 layout.Children.Add(chords);
+                layout.Children.Add(searchHeader);
+                layout.Children.Add(search);
                 page.Content = layout;
             }
 
